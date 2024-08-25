fde1a48e84337af159cbcd0b7253113437cb557a
Muzzamil Brohi <muzzamilaptech@gamil.com>
2024-08-24 19:45:31 -0700

    Protfolio

---
+++ script.js
@@ -0,0 +1,111 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+
+
+
+
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+  strings: ["Designer", "Coder","Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+});
+
+const body = document.querySelector("body"),
+toggleSwitch = document.getElementById("toggle-switch");
+
+toggleSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+})
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true
+});
+
+/* -- HOME -- */
+sr.reveal(".featured-text-card", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
+
+/* -- PROJECT BOX -- */
+sr.reveal(".project-box", { interval: 200 });
+
+/* -- HEADINGS -- */
+sr.reveal(".top-header", {});
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true
+});
+
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true
+});
+
+srRight.reveal(".skill", { delay: 100 });
+srRight.reveal(".skill-box", { delay: 100 });
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+function downloadCV() {
+  const link = document.createElement('a'); // Create a new anchor element
+  link.href = 'CV_Muzzamil.pdf'; // Set the href to your PDF file
+  link.download = 'CV_Muzzamil.pdf'; // Set the download attribute
+  document.body.appendChild(link); // Append the link to the body
+  link.click(); // Programmatically click the link to trigger the download
+  document.body.removeChild(link); // Remove the link after downloading
+}
